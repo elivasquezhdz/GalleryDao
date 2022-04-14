c65c8b9fa9796731e360c9f641c87be3407cc549
elivasquezhdz <eli.vasquezhdz@gmail.com>
2022-04-14 12:00:46 -0700

    first commig

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Slide } from "react-slideshow-image";
+import "./styles.css";
+import "react-slideshow-image/dist/styles.css";
+import { QRCode } from 'react-qrcode-logo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.slideRef = React.createRef();
+    this.back = this.back.bind(this);
+    this.next = this.next.bind(this);
+    this.state = {
+      current: 0
+    };
+  }
+
+  back() {
+    this.slideRef.current.goBack();
+  }
+
+  next() {
+    this.slideRef.current.goNext();
+  }
+
+  render() {
+    const properties = {
+      duration: 5000,
+      autoplay: true,
+      transitionDuration: 2000,
+      arrows: false,
+      infinite: true,
+      easing: "ease",
+      //indicators: (i) => <div className="indicator">{i + 1}</div>
+    };
+    const slideImages = [
+      {url:"https://lh3.googleusercontent.com/Pa5SMf7c2ySM4hD3uV6XCaYLqOuzsmEOLybLd3vrxMhaq0jAUm6ypQIDOzcG9o0i1nO4g3eqjky0OxBI9zSCY1QTax0KYCIz64EvSQ=w600"},
+    ];
+
+    const nfts =[
+
+
+      {contractAddress:"0x78c7549716a6290bd92005eb2a4da9701599428a",
+      tokenId:"212",
+      url:"https://opensea.io/assets/0x78c7549716a6290bd92005eb2a4da9701599428a/212"}, 
+  
+      {contractAddress:"0x495f947276749ce646f68ac8c248420045cb7b5e",
+      tokenId:"33911254874501868563329839216997312893782774976452525840755688063768262082561",
+      url:"https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/33911254874501868563329839216997312893782774976452525840755688063768262082561/"}, 
+     
+      {contractAddress:"0x495f947276749ce646f68ac8c248420045cb7b5e",
+      tokenId:"98005874063086952784788628439197826676066382464840560618238063422053382356993"},
+      
+      {contractAddress:"0x495f947276749ce646f68ac8c248420045cb7b5e",
+      tokenId:"98005874063086952784788628439197826676066382464840560618238063437446545145857"},
+
+  ];
+
+
+    return (
+      <div className="App">
+        <div>
+          <Slide ref={this.slideRef} {...properties}>
+          {nfts.map((each,index) => (
+            
+              <div  key={index}>
+                 <br></br>
+                <nft-card
+                contractAddress={each.contractAddress}
+                tokenId={each.tokenId}
+                width = "100%">
+                </nft-card>
+                <br></br>
+        <div  className="flex-container">
+          <QRCode url="https://webisora.com" />                
+        </div>
+        <div  className="flex-container">
+          <div><img src="/atlas.jpg" width="200"/></div>
+        </div>
+              </div>
+              
+          ))}
+          </Slide>
+        </div>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/example2.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+
+
+
+
+
+const slideImages = [
+  {
+    url: "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    caption: 'Slide 1'
+  },
+  {
+    url: "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+    caption: 'Slide 2'
+  },
+  {
+    url: "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    caption: 'Slide 3'
+  },
+];
+
+const Slideshow = () => {
+    return (
+      <div className="slide-container">
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div className="each-slide" key={index}>
+              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+              <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+    )
+}
+
+export default Slideshow;
